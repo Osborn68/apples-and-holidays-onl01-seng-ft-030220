5e7c3d1a3b012d63c65e8b24486757330a18c054
Christopher Osborn <z68ozz@gmail.com>
2020-03-12 18:02:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,8 +72,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 holiday_hash.map do |season, holidays|
- holidays.map do |holiday_name, supplies|
-    supplies.include?("BBQ") ? holiday_name : nil
+ holidays.map do |holiday, supplies|
+    holiday if supplies.include?("BBQ")
 end
 end.flatten.compact 
 end
